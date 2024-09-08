585bd942ee7ac622bb32b4678c01f730d1b65be0
Drew-drashy <107747111+Drew-drashy@users.noreply.github.com>
2024-09-08 21:52:32 +0530

    Delete frontend directory

--- frontend/src/components/login.js
+++
@@ -1,8 +0,0 @@
-import {useState} from 'react';
-import axios from 'axios';
-function login(){
-    const [email,setemail]=useState('');
-    const [password, setpassword]=useState('');
-    
-}
-export default login;
