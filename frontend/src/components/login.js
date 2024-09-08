dfe852cb7c19195a6fc0b1d85db3db48a5f47b3b
Drew-drashy <107747111+Drew-drashy@users.noreply.github.com>
2024-09-08 20:37:26 +0530

    "intial-phase"

---
+++ frontend/src/components/login.js
@@ -0,0 +1,8 @@
+import {useState} from 'react';
+import axios from 'axios';
+function login(){
+    const [email,setemail]=useState('');
+    const [password, setpassword]=useState('');
+    
+}
+export default login;
